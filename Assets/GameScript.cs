262b3ee8b940892e4705d598f430021e67f048f2
andrewepeter <64112518+andrewepeter@users.noreply.github.com>
2021-10-20 15:14:43 -0400

    Created Basic Menu + Esc to quit game
    Created a basic Menu that allows the user to play, see the rules, or quit. I also added the ability to press escape to stop playing.

--- Assets/GameScript.cs
+++ Assets/GameScript.cs
@@ -49,6 +49,11 @@ public class GameScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
         movePiece();
     }
 
---
+++ Assets/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartGame ()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void Rules ()
+    {
+
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+
+}
