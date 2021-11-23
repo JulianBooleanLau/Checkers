6353e4d4cea8d29e8557d117acbfe393fd871bf9
David Rapala-Escorcia <drapalae@uoguelph.ca>
2021-11-23 15:52:34 -0500

    Added camera rotation around checker board for the menu screens

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+        
+    }
+}
